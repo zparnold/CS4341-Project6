06dcbfcbc561b7415435a99a0d68cc559fc8756a
unknown <zparnold@GL2-03.admin.wpi.edu>
2015-03-02 20:49:20 -0500

    question 1 already answered, question 2 coming right now.

--- generate_data.m
+++ generate_data.m
@@ -2,7 +2,7 @@ function sampdata = generate_data(bnet,num_samples)
 
 % set random seed for reproducibility
 stream = RandStream('mt19937ar');
-RandStream.setDefaultStream(stream);
+RandStream.setGlobalStream(stream);
 
 N = size(bnet.dnodes,2);
 
@@ -12,7 +12,7 @@ sampdata = cell(num_samples,N);
 for n=1:num_samples
  samp = sample_bnet(bnet);
  % keep all the sampled data
- sampdata(n,1:N) = samp(1:N);
+ sampdata(n,6:N) = samp(6:N);
 end
 
 
